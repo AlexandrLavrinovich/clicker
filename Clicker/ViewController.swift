b3de5bbd4a42359e4445e389c4c5c5296821b174
Dinolavr <Dinolavr@users.noreply.github.com>
2019-11-04 22:01:23 +0500

    Initial Commit

---
+++ Clicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clicker
+//
+//  Created by MacBook Pro on 04.11.2019.
+//  Copyright © 2019 MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
